4fac0ec7382f2ad3fe162f230ad2d2f988b7dfd3
mozhutang <mozhutang@gmail.com>
2024-07-08 18:57:00 -0700

    Update productsSlice.ts

--- frontend/src/store/productsSlice.ts
+++ frontend/src/store/productsSlice.ts
@@ -360,15 +360,6 @@ const productsSlice = createSlice({
             state.cart[userId].push({ ...p1, discount });
           }
         });
-        state.products.forEach(p => {
-          console.log(p.name, p.cartQuantity);
-        })
-        console.log('---')
-        // @ts-ignore
-        state.cart[userId].forEach(p => {
-          console.log(p.name,p.cartQuantity);
-        })
-
       });
 
 }
